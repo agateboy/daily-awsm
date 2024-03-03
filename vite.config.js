caa22371381ffc1246e1621b00265aa0042f6369
agateboy <128492618+agateboy@users.noreply.github.com>
2024-03-03 22:40:41 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  baseUrl: '/daily-awsm/',
+  base: '/daily-awsm/',
   plugins: [react()],
 })
